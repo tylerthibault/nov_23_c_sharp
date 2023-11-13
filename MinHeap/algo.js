a982826f73d87d3a9efbaab6b0ab1d87918cd405
atbocompany <132010032+atbocompany@users.noreply.github.com>
2023-11-13 08:52:38 -0700

    w4d1 upload algos

--- MinHeap/algo.js
+++ MinHeap/algo.js
@@ -44,7 +44,9 @@ class MinHeap {
    * - Space: O(1) constant.
    * @returns {?number} Null if empty.
    */
-  top() { }
+  top() {
+    return this.heap.length > 1 ? this.heap[1] : null;
+  }
 
   /**
    * Inserts a new number into the heap and maintains the heaps order.
@@ -55,7 +57,32 @@ class MinHeap {
    * - Space: O(1) constant.
    * @param {number} num The num to add.
    */
-  insert(num) { }
+  insert(num) {
+    this.heap.push(num);
+    this.shiftUp();
+    // .push on array returns the new length
+    return this.size();
+  }
+
+  shiftUp() {
+    let idxOfNodeToShiftUp = this.heap.length - 1;
+
+    while (idxOfNodeToShiftUp > 1) {
+      const idxOfParent = this.idxOfParent(idxOfNodeToShiftUp);
+
+      const isParentSmallerOrEqual =
+        this.heap[idxOfParent] <= this.heap[idxOfNodeToShiftUp];
+
+      // Parent is supposed to be smaller so ordering is complete.
+      if (isParentSmallerOrEqual) {
+        break;
+      }
+
+      this.swap(idxOfNodeToShiftUp, idxOfParent);
+      // after swapping the node is at idxOfParent now.
+      idxOfNodeToShiftUp = idxOfParent;
+    }
+  }
 
   /**
    * Logs the tree horizontally with the root on the left and the index in
